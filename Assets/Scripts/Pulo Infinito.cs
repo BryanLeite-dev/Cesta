1b0b835388d42245924cd99647e5dcf432513be8
BryanLeite-dev <bryan.leite@etc.sp.gov.br>
2021-05-24 19:35:21 -0300

    Inclusão do Projeto

---
+++ Assets/Scripts/Pulo Infinito.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuloInfinito : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vector3 puloInfinito;
+
+    void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(puloInfinito, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+    
---
+++ Assets/Scripts/StartForce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    
+public class StartForce : MonoBehaviour
+{ 
+    public Vector3 startForce;
+
+    void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(startForce, ForceMode.Impulse);
+    }
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}
